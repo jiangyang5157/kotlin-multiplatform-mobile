6e6bf0d826424b4ec1b0ddd29c72011d713a811a
Yang <jiangyang5157@gmail.com>
2022-10-20 22:12:15 +1300

    na

--- kotlin-multiplatform-app/settings.gradle.kts
+++ kotlin-multiplatform-app/settings.gradle.kts
@@ -14,23 +14,28 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "kotlin-multiplatform-app"
+
 include(":shared")
+include(":androidApp")
 
 include(":kit")
 include(":common")
+
 include(":adapter")
+include(":demo-adapter")
+
 include(":video")
+include(":demo-video")
+
 include(":router")
+include(":demo-router")
+
 include(":widget")
 
-include(":androidApp")
-include(":demo-adapter")
-include(":demo-video")
-include(":demo-router")
-include(":demo-compose")
 include(":transaction_domain")
 include(":transaction_data_1")
 include(":transaction_data_2")
 include(":transaction_presentation_base")
 include(":transaction_presentation_1")
 include(":transaction_presentation_2")
+
