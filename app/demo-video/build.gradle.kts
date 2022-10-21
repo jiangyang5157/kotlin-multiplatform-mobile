f0ecfc0d135112edd4a2cd754de29a0bdff36522
Yang <jiangyang5157@gmail.com>
2022-10-22 00:41:27 +1300

    to .kts

---
+++ app/demo-video/build.gradle.kts
@@ -0,0 +1,64 @@
+plugins {
+    id("kotlin-android")
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.gmail.jiangyang5157.demo_video"
+    compileSdk = Config.CompileSdkVersion
+
+    defaultConfig {
+        minSdk = Config.MinSdkVersion
+        targetSdk = Config.TargetSdkVersion
+        applicationId = "com.gmail.jiangyang5157.demo_video"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        versionCode = Config.VersionCode
+        versionName = Config.VersionName
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android.txt"),
+                file("proguard-rules.pro")
+            )
+        }
+    }
+
+    lint {
+        abortOnError = false
+        lintConfig = rootProject.file("lint.xml")
+        htmlReport = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = Config.KotlinJvmTarget
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation(Dep.Kotlin)
+    implementation(Dep.Appcompat)
+    implementation(Dep.Material)
+    implementation(Dep.Constraintlayout)
+    implementation(Dep.Picasso)
+
+    testImplementation(Dep.Junit)
+
+    androidTestImplementation(Dep.AndroidxJunit)
+    androidTestImplementation(Dep.TestRunner)
+
+    // Internal
+    implementation(project(":video"))
+}
