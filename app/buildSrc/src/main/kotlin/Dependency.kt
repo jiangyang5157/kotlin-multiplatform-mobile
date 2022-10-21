a6a1c9449c7b49640a6874de6c59d90c8f14e901
Yang <jiangyang5157@gmail.com>
2022-10-21 15:29:46 +1300

    groovy2kotlin

--- app/buildSrc/src/main/kotlin/Dependency.kt
+++ app/buildSrc/src/main/kotlin/Dependency.kt
@@ -10,7 +10,6 @@ object Config {
     const val VersionCode = 1
     const val VersionName = "1.0"
 
-    val JavaVersion = org.gradle.api.JavaVersion.VERSION_1_8
     const val JavaCompatibility = "1.8"
     const val KotlinJvmTarget = "1.8"
     const val KotlinCompilerExtVersion = "1.3.2"
---
+++ app/kit/build.gradle.kts
@@ -0,0 +1,35 @@
+plugins {
+    kotlin("jvm")
+    `java-library`
+    `maven-publish`
+}
+
+publishing {
+    publications {
+        repositories {
+            maven {
+                url = uri(layout.buildDirectory.dir("maven"))
+            }
+        }
+        create<MavenPublication>("maven") {
+            groupId = "com.gmail.jiangyang5157"
+            artifactId = "kit"
+            version = "1.0"
+
+            from(components["java"])
+        }
+    }
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation(Dep.Kotlin)
+    implementation(Dep.Gson)
+
+    testImplementation(Dep.KotlinTestJunit)
+}
