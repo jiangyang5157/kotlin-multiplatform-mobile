32b8c13360c8c7f24c3587b2c66ec0dd445e666c
Yang <jiangyang5157@gmail.com>
2022-10-22 19:51:26 +1300

    na

---
+++ app/transaction_domain_1/build.gradle.kts
@@ -0,0 +1,66 @@
+plugins {
+    id("kotlin-android")
+    id("com.android.library")
+    id("kotlin-kapt")
+}
+
+android {
+    namespace = "com.gmail.jiangyang5157.transaction_domain"
+    compileSdk = Config.CompileSdkVersion
+
+    defaultConfig {
+        minSdk = Config.MinSdkVersion
+        targetSdk = Config.TargetSdkVersion
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android.txt"),
+                file("proguard-rules.pro")
+            )
+        }
+    }
+
+    lint {
+        abortOnError = false
+        lintConfig = rootProject.file("lint.xml")
+        htmlReport = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = Config.KotlinJvmTarget
+    }
+}
+
+dependencies {
+    implementation(Dep.Kotlin)
+    implementation(Dep.CoreKtx)
+    implementation(Dep.Appcompat)
+    implementation(Dep.Material)
+    implementation(Dep.Gson)
+    implementation(Dep.CoroutinesCore)
+
+    testImplementation(Dep.Junit)
+
+    androidTestImplementation(Dep.AndroidxJunit)
+    androidTestImplementation(Dep.EspressoCore)
+
+    // Internal
+    implementation(project(":kit"))
+    implementation(project(":common"))
+    api(project(":transaction_domain"))
+
+    // Database
+    implementation(Dep.Room)
+    implementation(Dep.RoomKtx)
+    kapt(Dep.RoomCompiler)
+}
