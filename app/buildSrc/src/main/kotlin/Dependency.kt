0c8be7fa659c0c4b48c740d48e44f2bfb82aaa9c
Yang <jiangyang5157@gmail.com>
2023-06-21 10:32:15 +1200

    MinSdkVersion = 24

--- app/buildSrc/src/main/kotlin/Dependency.kt
+++ app/buildSrc/src/main/kotlin/Dependency.kt
@@ -4,7 +4,7 @@ object Config {
 
     const val CompileSdkVersion = 33
     const val TargetSdkVersion = 33
-    const val MinSdkVersion = 23
+    const val MinSdkVersion = 24
 
     const val VersionCode = 1
     const val VersionName = "1.0"
