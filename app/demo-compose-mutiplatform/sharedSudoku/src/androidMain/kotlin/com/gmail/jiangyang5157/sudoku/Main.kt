00b7d0b256134fd2d45215f27c32f09b7078baf9
YangJ <jiangyang5157@gmail.com>
2023-07-04 15:07:15 +1200

    remove cm-s

--- app/demo-compose-mutiplatform/sharedSudoku/src/androidMain/kotlin/com/gmail/jiangyang5157/sudoku/Main.kt
+++
@@ -1,9 +0,0 @@
-package com.gmail.jiangyang5157.sudoku
-
-import androidx.compose.runtime.Composable
-
-actual fun getPlatform(): String =
-    "Android ${android.os.Build.VERSION.SDK_INT}"
-
-@Composable
-fun MainView() = App()
--- app/demo-compose-mutiplatform/sharedSudoku/src/iosMain/kotlin/com/gmail/jiangyang5157/sudoku/Main.kt
+++
@@ -1,9 +0,0 @@
-package com.gmail.jiangyang5157.sudoku
-
-import androidx.compose.ui.window.ComposeUIViewController
-import platform.UIKit.UIDevice
-
-actual fun getPlatform(): String =
-    "${UIDevice.currentDevice.systemName()} ${UIDevice.currentDevice.systemVersion}"
-
-fun MainViewController() = ComposeUIViewController { App() }
