6481fb5b581705eecbe79d36cf47513d30efeced
YangJ <jiangyang5157@gmail.com>
2023-07-04 13:50:57 +1200

    clean

--- app/test-1/clean.sh
+++ app/test-1/clean.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+rm -rf .idea
+./gradlew clean
+rm -rf .gradle
+rm -rf build
+rm -rf */build
+
 rm -rf iosSudoku/iosSudoku.xcworkspace
 rm -rf iosSudoku/iosSudoku.xcodeproj/project.xcworkspace
 rm -rf iosSudoku/iosSudoku.xcodeproj/xcuserdata
