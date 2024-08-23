6372788d2374ed797c1a74f3186a3346f3e4e02e
Yang Jiang <yangjiang@YangdeMacBook-Pro.local>
2024-08-24 10:51:41 +1200

    versions

--- app/settings.gradle.kts
+++ app/settings.gradle.kts
@@ -19,30 +19,30 @@ include(":kit") // kotlin library
 
 include(":common")
 
-//include(":adapter")
-//include(":demo-adapter")
-//
-//include(":video")
-//include(":demo-video")
-//
-//include(":router")
-//include(":demo-router")
-//
-//include(":widget")
-//
-//include(":demo-compose")
-//include(":demo-compose-canvas")
+include(":adapter")
+include(":demo-adapter")
 
-include(":transaction_domain_base") // kotlin library
-include(":transaction_domain_1")
-include(":transaction_domain_2") // kotlin library
-include(":transaction_data_1")
-include(":transaction_data_2")
+include(":video")
+include(":demo-video")
+
+include(":router")
+include(":demo-router")
+
+include(":widget")
+
+include(":demo-compose")
+include(":demo-compose-canvas")
+
+//include(":transaction_domain_base") // kotlin library
+//include(":transaction_domain_1")
+//include(":transaction_domain_2") // kotlin library
+//include(":transaction_data_1")
+//include(":transaction_data_2")
 //include(":transaction_presentation_base")
 //include(":transaction_presentation_1")
 //include(":transaction_presentation_2")
 //include(":demo-transaction")
 
-//include(":shared-common") // kmm
-//include(":shared-puzzle") // kmm
-//include(":demo-sudoku")
+include(":shared-common") // kmm
+include(":shared-puzzle") // kmm
+include(":demo-sudoku")
